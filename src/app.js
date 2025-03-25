0a02745ffe6025d7de3f71e58b8e360c299812eb
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-03-25 00:31:11 -0300

    structure in construction

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { Routes } from 'react-auto-import2'
+
+const App = () => (
+  <BrowserRouter>
+    { Routes }
+  </BrowserRouter>
+);
+
+export default App;
