5a2566767d4935ca524798b4c904ea9ef1fd0641
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-03-31 23:26:35 -0300

    edit sintax

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Config, Routes as Test } from 'react-auto-import2'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { Config, Routes as routes } from 'react-auto-import2'
 
-Config({
-  Routes: {
-    dir: '/src/routes2',
-  },
-});
-
-console.log('[Result]=> ', Test)
+console.log('[Result]=> ', Routes)
 
 createRoot(document.querySelector('#root'))
   .render(
     <BrowserRouter>
       <Routes>
-        {/* <Route path={ '/' } element={ <a>Page /</a> } /> */}
-        { Test }
+        { routes }
       </Routes>
     </BrowserRouter>
   );
